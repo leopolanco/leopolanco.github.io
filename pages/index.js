b200f2584d314fd602bb199d4a629b9bed556405
leo polanco <leo.polanco.n@gmail.com>
2021-01-16 21:54:00 -0400

    Saving2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 const Index = () => {
   return (
     <div>
-      <p>WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</p>
+      <p>WWWWWWWWWWWWWWWWWWWW</p>
     </div> 
       
      
