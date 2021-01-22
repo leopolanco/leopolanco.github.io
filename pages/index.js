f877e91d5f6f6725bb3887106530bb62d5d15e07
leo polanco <leo.polanco.n@gmail.com>
2021-01-22 12:13:28 -0400

    Nightly 1/22

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,35 @@
 import styles from '../styles/index.module.scss'
-import {LinkedIn, Mail, GitHub, MainImage} from '../public/assets/svg/svg'
-const greetings = 'Hello. I am Leo,'; //we're defining these for when we'll have to make a spanish version
+import {
+  LinkedIn, 
+  Mail, 
+  GitHub, 
+  MainImage,
+  ReactIcon,
+  NodeIcon,
+  MongoIcon,
+  SassIcon,
+  NextIcon
+  } from '../public/assets/svg/svg'
+{/*To do, make the icons travel trough the whole page */}
+//we're defining these for when we'll have to make a spanish version
+const greetings = 'Hello. I am Leo,'; 
 const description = 'fullstack developer';
 const smallDescription = 'Specialized in creating experiences and quality products with attention to the minimum detail.';
+const descriptiveText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+
+
 const Index = () => {
   
   return (
+    <>
     <div className={styles.mainDiv}>
+
+      {/* First section */}
       <div className={styles.leftContainer}>
         <div className={styles.greetings}>{greetings}</div>
         <div className={styles.description}><span>a</span> {description}</div>
         <div className={styles.smallDescription}>{smallDescription}</div>
       </div>
-
       <div >
         <div className={styles.rightContainer}>
           <div className={`${styles.mainImage} ${styles.showForDesktop}`}><MainImage/></div> {/*This image is called source code, it is a placeholder */}
@@ -22,8 +39,29 @@ const Index = () => {
             <a className={styles.mailIcon} target="_blank" rel="noopener" href='mailto:leo@leopolanco.com'><Mail/></a>
           </div>
         </div>
-      </div> 
-    </div> 
+      </div>
+    </div>
+
+
+        {/* Second section */}
+    <div className={styles.secondDiv}>
+      <div className={styles.aboutMeTitle}>About Me</div>
+      <div className={styles.aboutMe}>
+        <div className={styles.aboutMeText}>{descriptiveText}</div>
+        <div className={styles.aboutMeIcons}>
+          <div className={styles.aboutMeIcon}><NodeIcon/><div>NODEJS</div></div>
+          <div className={styles.aboutMeIcon}><ReactIcon/><div>REACT</div></div>
+          <div className={styles.aboutMeIcon}><MongoIcon/><div>MONGODB</div></div>
+          <div className={styles.aboutMeIcon}><NextIcon/><div>NEXTJS</div></div>
+          <div className={styles.aboutMeIcon}><SassIcon/><div>SASS</div></div>
+        </div>
+      </div>
+    </div>
+    
+    
+
+    
+    </>
   )
 }
 export default Index;
