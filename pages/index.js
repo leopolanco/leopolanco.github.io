71e9f6eca7796ec6656d89e6226d3f488c4d6825
leo polanco <leo.polanco.n@gmail.com>
2021-01-24 23:19:26 -0400

    Nightly 1/24

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Presentation from './components/Index/Presentation'
 {/*To do, make the icons travel trough the whole page */}
 //we're defining these for when we'll have to make a spanish version
 
-const descriptiveText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+const descriptiveText = 'Lorem ipsum dlor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
 
 
 const Index = () => {
