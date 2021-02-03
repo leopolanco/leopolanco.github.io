7c68f300f97b3365bda04592875e2f48b7fd3d47
leo polanco <leo.polanco.n@gmail.com>
2021-02-03 09:40:35 -0400

    Contact page done

--- pages/contact.js
+++ pages/contact.js
@@ -1,108 +1,150 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import _JSXStyle from 'styled-jsx/style'
-import {Loading, DoneIcon} from '../public/assets/svg/svg'
+import {
+  Loading,
+  DoneIcon,
+  Telegram,
+  WhatsApp,
+  Mail,
+  ContactImage
+} from '../public/assets/svg/svg'
 import styles from '../styles/contact.module.scss'
 
 const contact = () => {
-const [form, setForm] = useState({});
-const [message, setMessage] = useState('')
-const [email, setEmail] = useState('')
-const [userMessage, setUserMessage] = useState('')
+  const [form, setForm] = useState({})
+  const [message, setMessage] = useState('')
+  const [email, setEmail] = useState('')
+  const [userMessage, setUserMessage] = useState('')
 
-//we a use a loading part
-//because we're sending the data through a third party proxy
-//and it takes a bit to send it correctly and can fail for connection issues
-//this is registered in usermesssage
+  //we a use a loading part
+  //because we're sending the data through a third party proxy
+  //and it takes a bit to send it correctly and can fail for connection issues
+  //this is registered in usermesssage
 
-const [error, setError] = useState('')
+  const [error, setError] = useState('')
 
-useEffect(() => { 
-    setForm ({
-        message,
-        email
+  useEffect(() => {
+    setForm({
+      message,
+      email
     })
-} , [message,email])
+  }, [message, email])
 
- const sendFormToGoogleForms = () => {
+  const sendFormToGoogleForms = () => {
     const formData = new FormData()
 
     const GOOGLE_FORM_MESSAGE_ID = 'entry.973126710' //1er name
     const GOOGLE_FORM_EMAIL_ID = 'entry.1358619410' //2do name
-    const GOOGLE_FORM_ACTION_URL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfGeTApRi1L42OiaHWdhYlLR20m5sgCgDRYZpfqGlKyoiOF2g/formResponse'
+    const GOOGLE_FORM_ACTION_URL =
+      'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfGeTApRi1L42OiaHWdhYlLR20m5sgCgDRYZpfqGlKyoiOF2g/formResponse'
     const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
 
-    formData.append(GOOGLE_FORM_MESSAGE_ID, form.message);
-    formData.append(GOOGLE_FORM_EMAIL_ID, form.email);
-    axios.post(CORS_PROXY+GOOGLE_FORM_ACTION_URL, formData)
-    .then(()=>{
-        setUserMessage(<DoneIcon/>)
-    })
-    .catch((e)=>{
+    formData.append(GOOGLE_FORM_MESSAGE_ID, form.message)
+    formData.append(GOOGLE_FORM_EMAIL_ID, form.email)
+    axios
+      .post(CORS_PROXY + GOOGLE_FORM_ACTION_URL, formData)
+      .then(() => {
+        setUserMessage(<DoneIcon />)
+      })
+      .catch((e) => {
         setError('There was an error, please send your message again')
-    })
-}
+      })
+  }
 
-const onSubmit = (e) =>{
+  const onSubmit = (e) => {
     e.preventDefault()
-    
     //Validating
-    if(!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-    .test(email)) {
-        return setError('Invalid email')
-    } else if (message.length < 1){
-        return setError('Invalid message')
+    if (
+      !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+        email
+      )
+    ) {
+      return setError('Invalid email')
+    } else if (message.length < 1) {
+      return setError('Invalid message')
     }
-
     //Clearing fields
     setError('')
     setEmail('')
     setMessage('')
     //Sending
-    setUserMessage(<Loading/>) 
+    setUserMessage(<Loading />)
     sendFormToGoogleForms()
-}
+  }
 
-return (
+  return (
     <>
-
-<h3 className={styles.contactTitle}>Contact </h3>
-
-    <div className={styles.contactForm}>
-    <style global jsx>{`
+      <style global jsx>{`
         .header_navlink__1wJsj:nth-of-type(2),
         li:nth-child(2),
-        .footer_footerLinks__3_Agy>span:nth-child(2)
-         {
-            color: #ff2020;
+        .footer_footerLinks__3_Agy > span:nth-child(2) {
+          color: #ff2020;
         }
-    `}</style>
-        <span className={styles.contactFormTitle}>Send a message</span>
-        <form onSubmit={onSubmit} method="GET">
+      `}</style>
+      <h3 className={styles.contactTitle}>Contact </h3>
+
+      <div className={styles.contactContent}>
+        <div className={styles.contactForm}>
+          <span className={styles.contactFormTitle}>Send a message</span>
+          <form onSubmit={onSubmit} method='GET'>
             <div className={styles.emailInput}>
-        
-            <input value={email} onChange={(e)=> setEmail(e.target.value)} required/>
-            <label className={styles.emailLabel}>Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label className={styles.emailLabel}>Email</label>
             </div>
-            <div className={styles.messageInput} >
-            
-            <textarea value={message} onChange={(e)=> setMessage(e.target.value)} required></textarea>
-            <label className={styles.messageLabel}>Message</label>
+            <div className={styles.messageInput}>
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+              <label className={styles.messageLabel}>Message</label>
             </div>
             <div className={styles.contactFormBottom}>
-            {error ? <span>{error}</span> : <span>{userMessage}</span>}
-            <button className={styles.button} type="submit">Send</button>
-
+              {error ? <span>{error}</span> : <span>{userMessage}</span>}
+              <button className={styles.button} type='submit'>
+                Send
+              </button>
             </div>
-        </form>
-        
-    
-    
-    
-    
+          </form>
         </div>
+        <div className={styles.contactIcons}>
+          <ContactImage />
+          {/*  Placeholder image */}
+          <a
+            target='_blank'
+            rel='noopener'
+            alt='mail'
+            href='mailto:leo@leopolanco.com'
+          >
+            <Mail />
+            leo@leopolanco.com
+          </a>
+          <a
+            target='_blank'
+            rel='noopener'
+            alt='whatsapp'
+            href='https://wa.me/584246519234'
+          >
+            <WhatsApp /> +584246519234
+          </a>
+          <a
+            target='_blank'
+            rel='noopener'
+            alt='telegram'
+            href='https://t.me/Leopn'
+          >
+            <Telegram />
+            @leopn
+          </a>
+        </div>
+      </div>
     </>
-)}
+  )
+}
 
-
-export default contact;
+export default contact
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import RecentProjects from './components/Index/RecentProjects'
-import Langs from './components/Index/Langs';
+import Langs from './components/Index/Langs'
 import AboutMe from './components/Index/AboutMe'
 import Presentation from './components/Index/Presentation'
 /*To do:
 - put more content in aboutme and langs comp
 - make the recent projects redirect to projects page when clicked
-- improve the footer*/
+- improve the footer
+-please clean the code before finishing.*/
 
 const Index = () => (
-    <>
-    <Presentation/>
-    <AboutMe/>
-    <Langs/>
-    <RecentProjects/>
-    </>
+  <>
+    <Presentation />
+    <AboutMe />
+    <Langs />
+    <RecentProjects />
+  </>
 )
 
-export default Index;
+export default Index
