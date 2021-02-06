b6a680124b3b2dfb065a32735a0113ba1f59d2c7
leo polanco <leo.polanco.n@gmail.com>
2021-02-06 13:44:23 -0400

    Nightly 2/6

--- pages/projects.js
+++ pages/projects.js
@@ -1,27 +1,59 @@
 import _JSXStyle from 'styled-jsx/style'
 import styles from '../styles/projects.module.scss'
+import {
+  ReactIcon,
+  NodeIcon,
+  MongoIcon,
+  SassIcon,
+  HTMLIcon,
+  CSSIcon
+} from '../public/assets/svg/svg'
 //fill these manually.
 //don't need automation here as the projects will be sporadically added
 //best on top, next newest to oldest.
-const project = [
+const projectsArray = [
   {
-    title: '',
-    picture: '',
-    tech: '',
-    url: '',
-    description: ''
+    title: 'Proyectos Uptag',
+    picture: `/assets/images/Proyup1280.webp 1920w,
+    /assets/images/Proyup760.webp 1920w`,
+    tech: <>
+    <HTMLIcon/>
+    <CSSIcon/>
+    <NodeIcon/>
+    <MongoIcon/>
+          </>,
+    url: 'https://proyectosuptag.herokuapp.com',
+    description: `Application to store and review final projects, made for the uptag engineering departments`
   },
-  { title: '', picture: '', tech: '', url: '', description: '' },
-  { title: '', picture: '', tech: '', url: '', description: '' }
+  {
+    title: 'Real estate landing page',
+    picture: `/assets/images/REMock1280.webp 1920w,
+    /assets/images/REMock760.webp 1920w`,
+    tech: <>
+    <HTMLIcon/>
+    <CSSIcon/>
+    </>,
+    url: '',
+    description: `Mockup page for a real estate company, tested general design practices`
+  },
+  {
+    title: 'React test project',
+    picture: `/assets/images/BudgetApp1280.webp 1920w,
+    /assets/images/BudgetApp760.webp 1920w`,
+    tech: <>
+    <ReactIcon/>
+    <SassIcon/>
+    </>,
+    url: 'http://leo-react-project.herokuapp.com/',
+    description: `Application for budgeting. Can store, update and delete expenses, this was a course project`
+  },
 ]
 
-//each card will show the tech when hovered
-//the tech will be icons in a small bar below the picture
-// design will be:
-//Picture which will be the same heigh as the card and around 40% of the width
-// Title on the side and a small description below the title
+
 //play around with a border
 //need interactivity.
+const sizes = `(max-width: 45rem-0.1rem) 680px, 
+               (min-width: 45rem) 500px` //desktop size
 
 const projects = () => (
   <>
@@ -33,7 +65,41 @@ const projects = () => (
       }
     `}</style>
     <div className={styles.projectsTitle}>Projects</div>
-    <div className={styles.projectsContent}></div>
+    <div className={styles.projectsContent}>
+      {projectsArray.map((project,index) => {
+      let alt=`Project ${index+1}`;
+      return  (
+        <div key={`${project.title} ${index}`}>
+      <div className={styles.projectCard}>
+        <div className={styles.projectCardLeft}>
+          <div className={styles.projectPicture}>
+          <img
+          width='420px'
+
+            srcSet={project.picture}
+            sizes={sizes}
+            loading='lazy'
+            alt={alt}
+          />
+        <div className={styles.projectTech}>{project.tech}</div>
+          </div>
+          
+        </div>
+
+
+        <div className={styles.projectCardRight}>
+          <div className={styles.projectTitle}>{project.title}</div>
+          <div className={styles.projectDescription}>{project.description}</div>
+          {project.url && <a className={styles.projectUrl} target="_blank" rel="noopener" alt={alt} href={project.url}>VISIT</a> }
+        </div>
+        
+        
+      </div>
+      {index+1!=projectsArray.length && <hr className={styles.projectSeparator}/>}
+      
+      </div>
+      )})}
+    </div>
   </>
 )
 
