91e13d3d350e38f09dd621a94e7364233bae580f
leo polanco <leo.polanco.n@gmail.com>
2021-02-11 08:05:17 -0400

    Fixes to spacing and paragraphs

--- components/Index/AboutMe.js
+++ components/Index/AboutMe.js
@@ -1,5 +1,5 @@
 import styles from '../../styles/index.module.scss'
-const descriptiveText = `I am a Coro based developer, with 22 years old and a degree in IT from the UPTAG. 
+const descriptiveText = `I am a Coro based developer, I'm 22 years old and I have a degree in IT from the UPTAG. 
 I have been working with people all over the world in creating pages, apps and software.
 Passionate about programming and fitness, currently looking for a next challenge `  
 const farewell = `I hope you enjoy my page, Greetings.`
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Langs from '../components/Index/Langs'
 import AboutMe from '../components/Index/AboutMe'
 import Presentation from '../components/Index/Presentation'
 /*To do:
-- put more content in aboutme and langs comp
 - improve the footer
 -transition between pages
+- fix every spacing issue
 -please clean the code before finishing.*/
 
 const Index = () => (
