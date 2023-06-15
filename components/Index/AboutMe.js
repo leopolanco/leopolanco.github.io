cef44c7f72db55a7edf9f0449027dc3fe432c0ba
Leo Polanco <leo.polanco.n@gmail.com>
2023-06-15 18:58:30 -0400

    Update AboutMe.js

--- components/Index/AboutMe.js
+++ components/Index/AboutMe.js
@@ -1,4 +1,4 @@
-const descriptiveText = `I am a Coro based developer, I have a degree in IT from the UPTAG. 
+const descriptiveText = `I am a NY based developer, I have a degree in IT from the UPTAG (Coro, Venezuela). 
 I have been working with people all over the world in creating pages, apps and software.
 Passionate about programming and fitness, currently looking for a next challenge `
 const farewell = `I hope you enjoy my page, Greetings.`
